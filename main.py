7e581103cd75687610f64fc95543aac12d402016
isPrabhpuran <prabh@autonomixsolutions.com>
2026-01-06 17:08:11 +0530

    added today's progress

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from fastapi import FastAPI
 
 from pydantic import BaseModel
 
+from google import genai
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = FastAPI()
 
 class Item(BaseModel):
@@ -22,3 +28,13 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.get("/generate-text/")
+def generate_text():
+    client = genai.Client()
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Write a poem about FastAPI in Python."
+    )
+    return {response.text}
